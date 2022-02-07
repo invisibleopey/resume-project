58863007f9e2534880b69bbef0fa8aa1d946150d
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-07 16:40:17 +0100

    Test the education component

---
+++ src/tests/Education.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import Education from '../components/Education';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+it('renders Form', () => {
+  const props = { school: {} };
+  render(<Education school={props.school} />);
+  const form = screen.getByTestId('education-form');
+
+  expect(form).toBeInTheDocument();
+});
+
+it('receives and display the right props', () => {
+  const handleChange = jest.fn;
+  const props = { school: { name: 'Unilorin' }, handleChange: handleChange };
+  render(<Education school={props.school} handleChange={props.handleChange} />);
+
+  const schoolNameField = screen.getByPlaceholderText(/University Name/i);
+  expect(schoolNameField).toHaveAttribute('value', 'Unilorin');
+});
+
+it('changes value of input field', () => {
+  const props = { school: {} };
+  render(<Education school={props.school} />);
+
+  userEvent.type(screen.getByPlaceholderText(/University Name/i), 'UniOsun');
+  expect(screen.getByPlaceholderText(/University Name/i)).toHaveValue('UniOsun');
+});

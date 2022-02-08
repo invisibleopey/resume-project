884a0157a9d5627cf7de9dadfed5e44a4c95b813
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-08 01:34:23 +0100

    Import and render overview, fix some of the handler functions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import General from './components/General';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import Overview from './components/Overview';
 
 class App extends Component {
   constructor(props) {
@@ -49,12 +50,39 @@ class App extends Component {
 
   handleSchoolChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ school: { [name]: value } });
+    this.setState((prevState) => ({
+      school: {
+        ...prevState.school,
+        [name]: value,
+      },
+    }));
   };
 
   handleJobChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ job: { [name]: value } });
+    this.setState((prevState) => ({
+      job: {
+        ...prevState.job,
+        [name]: value,
+      },
+    }));
+  };
+
+  handleDelete = (event) => {
+    const targetId = event.target.parentNode.parentElement.id;
+    let deleteTarget;
+    // eslint-disable-next-line default-case
+    switch (event.target.parentNode.parentElement.className) {
+      case 'schools':
+        deleteTarget = 'schools';
+        break;
+      case 'jobs':
+        deleteTarget = 'jobs';
+        break;
+    }
+    this.setState({
+      [deleteTarget]: this.state[deleteTarget].filter((item) => item.id !== targetId),
+    });
   };
 
   handleSchoolSubmit = (event) => {
@@ -73,6 +101,28 @@ class App extends Component {
     });
   };
 
+  handleSchoolEdit = (event) => {
+    const targetId = event.target.parentNode.parentElement.id;
+    this.setState((prevState) => {
+      const targetSchool = prevState.schools.find((school) => school.id === targetId);
+      // The if statement is here to prevent throwing an error in cases where targetSchool is not found,
+      // therefore, to prevent trying to read properties from undefined
+      if (targetSchool)
+        return {
+          school: {
+            name: targetSchool.name,
+            city: targetSchool.city,
+            degree: targetSchool.degree,
+            subject: targetSchool.subject,
+            startDate: targetSchool.startDate,
+            endDate: targetSchool.endDate,
+            id: targetSchool.id,
+          },
+          schools: prevState.schools.filter((school) => school.id !== targetId),
+        };
+    });
+  };
+
   handleJobSubmit = (event) => {
     event.preventDefault();
     this.setState({
@@ -88,9 +138,41 @@ class App extends Component {
     });
   };
 
+  handleJobEdit = (event) => {
+    const targetId = event.target.parentNode.parentElement.id;
+    this.setState((prevState) => {
+      const targetJob = prevState.jobs.find((job) => job.id === targetId);
+      // The if statement is here to prevent throwing an error in cases where targetJob is not found,
+      // therefore, to prevent trying to read properties from undefined
+      if (targetJob)
+        return {
+          job: {
+            position: targetJob.position,
+            company: targetJob.company,
+            city: targetJob.city,
+            startDate: targetJob.startDate,
+            endDate: targetJob.endDate,
+            id: targetJob.id,
+          },
+          jobs: prevState.jobs.filter((job) => job.id !== targetId),
+        };
+    });
+  };
+
   render() {
-    const { firstName, lastName, title, address, email, phoneNumber, description, school, job } =
-      this.state;
+    const {
+      firstName,
+      lastName,
+      title,
+      address,
+      email,
+      phoneNumber,
+      description,
+      school,
+      job,
+      schools,
+      jobs,
+    } = this.state;
     return (
       <div className="App">
         <Header />
@@ -115,6 +197,22 @@ class App extends Component {
             handleChange={this.handleJobChange}
             handleJobSubmit={this.handleJobSubmit}
           />
+          <div className="overview-container">
+            <Overview
+              firstName={firstName}
+              lastName={lastName}
+              title={title}
+              address={address}
+              email={email}
+              phoneNumber={phoneNumber}
+              description={description}
+              schools={schools}
+              jobs={jobs}
+              handleDelete={this.handleDelete}
+              handleSchoolEdit={this.handleSchoolEdit}
+              handleJobEdit={this.handleJobEdit}
+            />
+          </div>
         </div>
         <Footer />
       </div>

507114e89a008bc6c76f488e9c69e673f6dfe0ab
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-10 00:05:18 +0100

    Remove clashing className from h1 tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="header">
-      <h1 className="header">Resume Generator</h1>
+      <h1>Resume Generator</h1>
     </div>
   );
 }

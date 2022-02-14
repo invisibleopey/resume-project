1ba331a54c7b279ab074c13963e4d99ff6e0dc04
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-15 00:42:36 +0100

    Fix the controlled state of general input forms

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import './styles/App.css';
 import Header from './components/Header';
@@ -35,228 +35,76 @@ const App = () => {
     id: uniqid(),
   });
   const [jobs, setJobs] = useState([]);
+
+  const handleSchoolChange = (event) => {
+    const { name, value } = event.target;
+    setSchool((prevState) => ({
+      ...prevState.school,
+      [name]: value,
+    }));
+  };
+
+  const handleJobChange = (event) => {
+    const { name, value } = event.target;
+    setJob((prevState) => ({
+      ...prevState.job,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="content">
+        <General
+          firstName={{ ...firstName }}
+          lastName={{ ...lastName }}
+          title={{ ...title }}
+          address={{ ...address }}
+          email={{ ...email }}
+          phoneNumber={{ ...phoneNumber }}
+          description={{ ...description }}
+        />
+        <Education
+          school={school}
+          handleChange={handleSchoolChange}
+          // handleSchoolSubmit={this.handleSchoolSubmit}
+        />
+        <Experience
+          job={job}
+          handleChange={handleJobChange}
+          // handleJobSubmit={this.handleJobSubmit}
+        />
+        <div>
+          <Overview
+            firstName={firstName.value}
+            lastName={lastName.value}
+            title={title.value}
+            address={address.value}
+            email={email.value}
+            phoneNumber={phoneNumber.value}
+            description={description.value}
+            schools={schools}
+            jobs={jobs}
+            // handleDelete={this.handleDelete}
+            // handleSchoolEdit={this.handleSchoolEdit}
+            // handleJobEdit={this.handleJobEdit}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 function useGeneralInput(initialValue) {
   const [value, setValue] = useState(initialValue);
 
-  function handleGeneralChange(event) {
-    setValue(e.target.value);
+  function handleChange(event) {
+    setValue(event.target.value);
   }
 
-  return value, handleGeneralChange;
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: '',
-      lastName: '',
-      title: '',
-      address: '',
-      email: '',
-      phoneNumber: '',
-      description: '',
-      school: {
-        name: '',
-        city: '',
-        degree: '',
-        subject: '',
-        startDate: '',
-        endDate: '',
-        id: uniqid(),
-      },
-      schools: [],
-      job: {
-        position: '',
-        company: '',
-        city: '',
-        startDate: '',
-        endDate: '',
-        id: uniqid(),
-      },
-      jobs: [],
-    };
-  }
-
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSchoolChange = (event) => {
-    const { name, value } = event.target;
-    this.setState((prevState) => ({
-      school: {
-        ...prevState.school,
-        [name]: value,
-      },
-    }));
-  };
-
-  handleJobChange = (event) => {
-    const { name, value } = event.target;
-    this.setState((prevState) => ({
-      job: {
-        ...prevState.job,
-        [name]: value,
-      },
-    }));
-  };
-
-  handleDelete = (event) => {
-    const targetId = event.target.parentNode.parentElement.id;
-    let deleteTarget;
-    // eslint-disable-next-line default-case
-    switch (event.target.parentNode.parentElement.className) {
-      case 'schools':
-        deleteTarget = 'schools';
-        break;
-      case 'jobs':
-        deleteTarget = 'jobs';
-        break;
-    }
-    this.setState({
-      [deleteTarget]: this.state[deleteTarget].filter((item) => item.id !== targetId),
-    });
-  };
-
-  handleSchoolSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      schools: this.state.schools.concat(this.state.school),
-      school: {
-        name: '',
-        city: '',
-        degree: '',
-        subject: '',
-        startDate: '',
-        endDate: '',
-        id: uniqid(),
-      },
-    });
-  };
-
-  handleSchoolEdit = (event) => {
-    const targetId = event.target.parentNode.parentElement.id;
-    this.setState((prevState) => {
-      const targetSchool = prevState.schools.find((school) => school.id === targetId);
-      // The if statement is here to prevent throwing an error in cases where targetSchool is not found,
-      // therefore, to prevent trying to read properties from undefined
-      if (targetSchool)
-        return {
-          school: {
-            name: targetSchool.name,
-            city: targetSchool.city,
-            degree: targetSchool.degree,
-            subject: targetSchool.subject,
-            startDate: targetSchool.startDate,
-            endDate: targetSchool.endDate,
-            id: targetSchool.id,
-          },
-          schools: prevState.schools.filter((school) => school.id !== targetId),
-        };
-    });
-  };
-
-  handleJobSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      jobs: this.state.jobs.concat(this.state.job),
-      job: {
-        position: '',
-        company: '',
-        city: '',
-        startDate: '',
-        endDate: '',
-        id: uniqid(),
-      },
-    });
-  };
-
-  handleJobEdit = (event) => {
-    const targetId = event.target.parentNode.parentElement.id;
-    this.setState((prevState) => {
-      const targetJob = prevState.jobs.find((job) => job.id === targetId);
-      // The if statement is here to prevent throwing an error in cases where targetJob is not found,
-      // therefore, to prevent trying to read properties from undefined
-      if (targetJob)
-        return {
-          job: {
-            position: targetJob.position,
-            company: targetJob.company,
-            city: targetJob.city,
-            startDate: targetJob.startDate,
-            endDate: targetJob.endDate,
-            id: targetJob.id,
-          },
-          jobs: prevState.jobs.filter((job) => job.id !== targetId),
-        };
-    });
-  };
-
-  render() {
-    const {
-      firstName,
-      lastName,
-      title,
-      address,
-      email,
-      phoneNumber,
-      description,
-      school,
-      job,
-      schools,
-      jobs,
-    } = this.state;
-    return (
-      <div className="App">
-        <Header />
-        <div className="content">
-          <General
-            firstName={firstName}
-            lastName={lastName}
-            title={title}
-            address={address}
-            email={email}
-            phoneNumber={phoneNumber}
-            description={description}
-            handleChange={this.handleChange}
-          />
-          <Education
-            school={school}
-            handleChange={this.handleSchoolChange}
-            handleSchoolSubmit={this.handleSchoolSubmit}
-          />
-          <Experience
-            job={job}
-            handleChange={this.handleJobChange}
-            handleJobSubmit={this.handleJobSubmit}
-          />
-          <div>
-            <Overview
-              firstName={firstName}
-              lastName={lastName}
-              title={title}
-              address={address}
-              email={email}
-              phoneNumber={phoneNumber}
-              description={description}
-              schools={schools}
-              jobs={jobs}
-              handleDelete={this.handleDelete}
-              handleSchoolEdit={this.handleSchoolEdit}
-              handleJobEdit={this.handleJobEdit}
-            />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+  return { value, handleChange };
 }
 
 export default App;
--- src/components/General.js
+++ src/components/General.js
@@ -11,43 +11,43 @@ function General(props) {
           <input
             type="text"
             name="firstName"
-            value={firstName}
-            onChange={handleChange}
+            value={firstName.value}
+            onChange={firstName.handleChange}
             placeholder="First Name"
           />
           <input
             type="text"
             name="lastName"
-            value={lastName}
-            onChange={handleChange}
+            value={lastName.value}
+            onChange={lastName.handleChange}
             placeholder="Last Name"
           />
           <input
             type="text"
             name="title"
-            value={title}
-            onChange={handleChange}
+            value={title.value}
+            onChange={title.handleChange}
             placeholder="Title"
           />
           <input
             type="text"
             name="address"
-            value={address}
-            onChange={handleChange}
+            value={address.value}
+            onChange={address.handleChange}
             placeholder="Address"
           />
           <input
             type="email"
             name="email"
-            value={email}
-            onChange={handleChange}
+            value={email.value}
+            onChange={email.handleChange}
             placeholder="Email"
           />
           <input
             type="text"
             name="phoneNumber"
-            value={phoneNumber}
-            onChange={handleChange}
+            value={phoneNumber.value}
+            onChange={phoneNumber.handleChange}
             placeholder="Phone Number"
           />
           <textarea
@@ -55,8 +55,8 @@ function General(props) {
             cols="10"
             rows="10"
             placeholder="Description"
-            value={description}
-            onChange={handleChange}
+            value={description.value}
+            onChange={description.handleChange}
           ></textarea>
         </fieldset>
       </form>
